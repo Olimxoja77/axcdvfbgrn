2109fc0701acd90ae0e4244c8a79bbc342365fb9
Olimxoja77 <odilxojaevolimxoja@gmail.com>
2024-11-18 16:45:31 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+// 1-salov  
+// let nula=5;
+// let nul2= 2;
+// 2-savol
+// alert(nula % nul2);
+// let n =Math.floor(Math.random()*10)
+// console.log(n);
+// 3-savol
+// let a =12.510
+// console.log(Math.trunc(a));
+// 4-savol
+//  let harflar = [
+//   "a",  "b",  "d",   "e","f",   "g",  "h",   "i",   "j",   "k","l", "m", "n",  "o",  "p",   "q",   "r",   "s",   "t",   "u", "v","x","y", "z",'o'
+//  ];
+//  console.log( harflar[24] +
+//     harflar[10] +
+//     harflar[7] +    
+//      harflar[11] +
+//    harflar[21] +
+//      harflar[24] +
+//     harflar[8] +
+//      harflar[0] );
+// 5 -savol
+// let d= prompt('yosha kiriting')
+// if(d>18){
+//     console.log('Siz kotasiz');
+    
+// }else{
+//     console.log('Siz kichiksiz');
+    
+// }
+// 6 -savol
+// let a = ''
+// let ism =prompt('ism kiriting')
+// for (let i = ism.length - 1; i >= 0; i--) {
+//        a = a + ism[i];
+//      }
+//      alert(a);
+// 7 -savol
+// let s =document.querySelector('h1')
+// let z =document.querySelector('h2')
+// let d =document.querySelector('h3')
+// let a = prompt('Ismingizni kiriting')
+// let b = prompt('Familiya kiriting')
+// let c = prompt('id kiriting')
+// s.innerHTML =a;
+// z.innerHTML =b;
+// d.innerHTML =c;
+
+
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let num1 = []
+// let num2= []
+// for (let i = 0; i < sonlar.length; i++) {
+//   if (sonlar % 2) {
+//     num1.push(sonlar[i])
+//   } else {
+//     num2.push(sonlar[i]);
+//   }
+// }
+// console.log(sonlar);
+// console.log(num1);
+// console.log(num2);
+
+ 
+    
+
+
